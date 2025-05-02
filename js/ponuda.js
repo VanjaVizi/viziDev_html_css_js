26201c6077c594d1819bbe16df314eccde9ae492
VanjaVizi <vanjazi999@gmail.com>
2025-05-02 12:41:31 +0200

    omoguceno osnovno dodavanje proizvoda u korpu

--- js/ponuda.js
+++ js/ponuda.js
@@ -1,6 +1,18 @@
+/////// 10- cokolada
+
+/// 1010
+/// 1020
+//  1030
+
+
+////20
+//2010
+//2020
+
+
 const proizvodi = [
     {
-        id:1,
+        id:10,
         slika:"https://as1.ftcdn.net/v2/jpg/06/92/12/82/1000_F_692128216_xUPMfetAt5gmKPh6K0nXXkfmgtUffvTA.jpg",
         naziv: "Kugla sladoleda od cokolade",
         opis: "Kugla sladoleda od cokolade sa komadicima cokolade",
@@ -9,7 +21,7 @@ const proizvodi = [
          
     },
     {
-        id:2,
+        id:20,
         slika:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvhWcCJbUESapVgTJ6gM26zEg_Kx3UEDbYpg&s",
         naziv: "Kugla sladoleda od vanile",
         opis: "Kugla sladoleda od vanile sa komadicima cokolade",
@@ -18,7 +30,7 @@ const proizvodi = [
          
     },
     {
-        id:3,
+        id:30,
         slika:"https://as1.ftcdn.net/v2/jpg/06/92/12/82/1000_F_692128216_xUPMfetAt5gmKPh6K0nXXkfmgtUffvTA.jpg",
         naziv: "Kugla sladoleda od jagode",
         opis: "Kugla sladoleda od cokolade sa komadicima cokolade",
@@ -27,7 +39,7 @@ const proizvodi = [
          
     },
     {
-        id:4,
+        id:40,
         slika:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvhWcCJbUESapVgTJ6gM26zEg_Kx3UEDbYpg&s",
         naziv: "Kugla sladoleda od karamele",
         opis: "Kugla sladoleda od vanile sa komadicima cokolade",
@@ -39,14 +51,20 @@ const proizvodi = [
 
 ];
 
+function dodajUkorpu(index) {
 
+    let korpa = JSON.parse(localStorage.getItem("korpa")) || [];
+    korpa.push(proizvodi[index]);
 
+    localStorage.setItem("korpa", JSON.stringify(korpa));
+    alert("Proizvod je dodat u korpu!");
 
+}
 function ucitajProizvode() {
     const sviProizvodiDiv= document.getElementById("sviproizvodi");
     sviProizvodiDiv.innerHTML = ""; // Očistimo div pre nego što dodamo nove proizvode
 
-    proizvodi.forEach(proizvod => {
+    proizvodi.forEach((proizvod,index) => {
         const proizvodDiv = document.createElement("div");
         proizvodDiv.classList.add("proizvod");
 
@@ -59,13 +77,19 @@ function ucitajProizvode() {
             <i class="fa-solid fa-tag"></i>
             <span class="staracena">Stara cena: ${proizvod.staraCena} RSD</span>
             <span class="novacena">Nova cena: ${proizvod.novaCena} RSD</span>
-            <button class="dodaj"> <i class="fa-solid fa-cart-plus"></i>  Dodaj u korpu</button>
+            <button class="dodaj" data-id= ${index}> <i class="fa-solid fa-cart-plus"></i>  Dodaj u korpu</button>
         `;
 
         sviProizvodiDiv.appendChild(proizvodDiv);
     });
 
-
+    const dugmici = document.querySelectorAll(".dodaj");
+    dugmici.forEach((dugme) => {
+        dugme.addEventListener("click", () => {
+            const index =  dugme.getAttribute("data-id");
+           dodajUkorpu(index);
+        });
+    });
 
 
 }
